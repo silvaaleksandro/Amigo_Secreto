d150c444c0dcfea95248ed029b1082d0aedb6cac
silvaaleksandro <silvaal@gmail.com>
2025-08-21 02:30:14 -0300

    Criar função para Agregar Amigos

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+
+function adicionarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    //console.log(`Texto = ${amigo}`);
+    if(amigo == "" || amigo == NaN){
+        alert("Por favor insira um nome.");
+    }else {
+        amigos.push(amigo);
+        //amigos.forEach(element => {console.log(element)});
+
+    }
+    document.getElementById("amigo").value = "";
+ 
+}
