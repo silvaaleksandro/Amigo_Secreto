80145ee0fea14c9a8173b5c16d3d11ec23e74bb5
silvaaleksandro <silvaal@gmail.com>
2025-08-21 03:22:43 -0300

    Criar funcao para preencher lista em tela

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ function adicionarAmigo(){
         alert("Por favor insira um nome.");
     }else {
         amigos.push(amigo);
+        criarLista();
         //amigos.forEach(element => {console.log(element)});
-
+        console.log(amigos);
     }
-    document.getElementById("amigo").value = "";
- 
+    document.getElementById("amigo").value = ""; 
+}
+
+function criarLista(){
+    lista = document.getElementById("listaAmigos") ;
+    lista.innerHTML = "";
+    
+    amigos.forEach(element => {
+        item = document.createElement("li");
+        item.innerHTML = element;
+        lista.appendChild(item);
+    });
 }
